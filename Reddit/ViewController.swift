4538690414cd862fbfeaec48e375f1d63653f5cb
Rutger Benoot <rutgerbenoot@hotmail.com>
2016-01-07 23:09:19 +0100

    Initial Commit

---
+++ Reddit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Reddit
+//
+//  Created by Rutger Benoot on 7/01/16.
+//  Copyright © 2016 Rutger Benoot. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
